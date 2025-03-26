7f11a9f34af03e2d3f104d292c70ce259063f451
dedok0x <dedok0x@yandex.ru>
2025-03-26 05:07:55 +0300

    Fix: lab4 | Исправлены лайки, кнопки теперь функциональны

--- lab4/lib/main.dart
+++ lab4/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:share_plus/share_plus.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,21 +20,24 @@ class DormitoryPage extends StatefulWidget {
 }
 
 class _DormitoryPageState extends State<DormitoryPage> {
-  int _likeCount = 0; // Начальное значение счетчика
+  int _likeCount = 0;
+  bool _isLiked = false;
 
-  void _incrementLikeCount() {
+  void _toggleLike() {
     setState(() {
-      _likeCount++;
+      if (_isLiked) {
+        _likeCount--;
+      } else {
+        _likeCount++;
+      }
+      _isLiked = !_isLiked;
     });
   }
 
   Future<void> _launchURL(String url) async {
-    try {
-      if (await canLaunchUrl(Uri.parse(url))) {
+    try{
         await launchUrl(Uri.parse(url));
-      } else {
-        throw 'Could not launch $url';
-      }
+
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Ошибка: $e')),
@@ -40,11 +45,44 @@ class _DormitoryPageState extends State<DormitoryPage> {
     }
   }
 
+  Future<void> _callPhone() async {
+    const url = 'https://yandex.ru/maps/org/obshchezhitiye_20/87985623421/';
+    await _launchURL(url);
+  }
+
+  Future<void> _openNavigation() async {
+    const url = 'https://yandex.ru/maps/org/obshchezhitiye_20/87985623421/?ll=38.920741%2C45.050329&z=16';
+    await _launchURL(url);
+  }
+
+  Future<void> _shareInfo() async {
+    const url = 'https://yandex.ru/maps/org/obshchezhitiye_20/87985623421/';
+    const text = 'Общежитие №20 КубГАУ - Краснодар, ул. Калинина, 13';
+    await Share.share('$text\n$url');
+  }
+
+  Widget _buildActionButton({
+    required IconData icon,
+    required String label,
+    required VoidCallback onPressed,
+    Color color = Colors.green,
+  }) {
+    return Column(
+      children: [
+        IconButton(
+          icon: Icon(icon, color: color, size: 30),
+          onPressed: onPressed,
+        ),
+        Text(label),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Общежитие КГАУ'),
+        title: Text('Общежитие КубГАУ'),
         backgroundColor: Colors.green,
       ),
       body: SingleChildScrollView(
@@ -52,7 +90,7 @@ class _DormitoryPageState extends State<DormitoryPage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Image.network(
-              'https://sun9-40.userapi.com/s/v1/ig2/m8wRvQ70t4diy--ngylsjzqPH0Q0zKd_zXn2MDsAG7jW6_HqWtmFZS9vQ-jA2h6Ybe842VvvBt7WkXUWPvdXymqG.jpg?quality=95&as=32x23,48x34,72x51,108x77,160x114,240x171,360x257,480x343,540x385,640x457,720x514,1080x771,1280x913,1435x1024&from=bu&u=vkdmz6YeWjWtphWD_EJyCAmvnI8spNDZd83OvRNVUpU&cs=604x431', // Замените на URL изображения
+              'https://sun9-40.userapi.com/s/v1/ig2/m8wRvQ70t4diy--ngylsjzqPH0Q0zKd_zXn2MDsAG7jW6_HqWtmFZS9vQ-jA2h6Ybe842VvvBt7WkXUWPvdXymqG.jpg?quality=95&as=32x23,48x34,72x51,108x77,160x114,240x171,360x257,480x343,540x385,640x457,720x514,1080x771,1280x913,1435x1024&from=bu&u=vkdmz6YeWjWtphWD_EJyCAmvnI8spNDZd83OvRNVUpU&cs=604x431',
               height: 200,
               width: double.infinity,
               fit: BoxFit.cover,
@@ -65,8 +103,11 @@ class _DormitoryPageState extends State<DormitoryPage> {
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   IconButton(
-                    icon: Icon(Icons.favorite, color: Colors.red),
-                    onPressed: _incrementLikeCount,
+                    icon: Icon(
+                      _isLiked ? Icons.favorite : Icons.favorite_border,
+                      color: _isLiked ? Colors.red : Colors.grey,
+                    ),
+                    onPressed: _toggleLike,
                   ),
                   Text('$_likeCount'),
                 ],
@@ -75,40 +116,20 @@ class _DormitoryPageState extends State<DormitoryPage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Column(
-                  children: [
-                    IconButton(
-                      icon: Icon(Icons.phone, color: Colors.green, size: 30),
-                      onPressed: () {
-                        _launchURL('https://yandex.ru/maps/org/obshchezhitiye_20/87985623421/?ll=38.920741%2C45.050329&z=7');
-                        _launchURL('https://yandex.ru/maps/org/obshchezhitiye_20/87985623421/?ll=38.920741%2C45.050329&z=7');
-                      },
-                    ),
-                    Text('Позвонить'),
-                  ],
+                _buildActionButton(
+                  icon: Icons.phone,
+                  label: 'Позвонить',
+                  onPressed: _callPhone,
                 ),
-                SizedBox(width: 30),
-                Column(
-                  children: [
-                    IconButton(
-                      icon: Icon(Icons.navigation, color: Colors.green, size: 30),
-                      onPressed: () {
-                      },
-                    ),
-                    Text('Маршрут'),
-                  ],
+                _buildActionButton(
+                  icon: Icons.navigation,
+                  label: 'Маршрут',
+                  onPressed: _openNavigation,
                 ),
-                SizedBox(width: 20),
-                Column(
-                  children: [
-                    IconButton(
-                      icon: Icon(Icons.share, color: Colors.green, size: 30),
-                      onPressed: () {
-                        _launchURL('https://yandex.ru/maps/org/obshchezhitiye_20/87985623421/?ll=38.920741%2C45.050329&z=7');
-                      },
-                    ),
-                    Text('Поделиться'),
-                  ],
+                _buildActionButton(
+                  icon: Icons.share,
+                  label: 'Поделиться',
+                  onPressed: _shareInfo,
                 ),
               ],
             ),
